f8b1d0fea02f10bfe06279cd32d53755ed62c177
Hendi Santika <hendisantika@gmail.com>
2018-10-05 05:08:38 +0700

    #15 - Add Sign Up Form Request

---
+++ src/main/java/com/hendisantika/springbootjwtauthentication/request/SignUpForm.java
@@ -0,0 +1,42 @@
+package com.hendisantika.springbootjwtauthentication.request;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import java.util.Set;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-jwt-authentication
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 05/10/18
+ * Time: 05.07
+ * To change this template use File | Settings | File Templates.
+ */
+@Getter
+@Setter
+public class SignUpForm {
+    @NotBlank
+    @Size(min = 3, max = 50)
+    private String name;
+
+    @NotBlank
+    @Size(min = 3, max = 50)
+    private String username;
+
+    @NotBlank
+    @Size(max = 60)
+    @Email
+    private String email;
+
+    private Set<String> role;
+
+    @NotBlank
+    @Size(min = 6, max = 40)
+    private String password;
+}
