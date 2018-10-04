a13c82641778bdbe36d464668d7a3d5df6a1b1dd
Hendi Santika <hendisantika@gmail.com>
2018-10-05 05:07:38 +0700

    #14 - Add Login Form

---
+++ src/main/java/com/hendisantika/springbootjwtauthentication/request/LoginForm.java
@@ -0,0 +1,30 @@
+package com.hendisantika.springbootjwtauthentication.request;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-jwt-authentication
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 05/10/18
+ * Time: 05.07
+ * To change this template use File | Settings | File Templates.
+ */
+
+@Getter
+@Setter
+public class LoginForm {
+    @NotBlank
+    @Size(min = 3, max = 60)
+    private String username;
+
+    @NotBlank
+    @Size(min = 6, max = 40)
+    private String password;
+}
