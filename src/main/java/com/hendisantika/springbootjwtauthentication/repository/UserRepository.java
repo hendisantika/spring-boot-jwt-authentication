b1596c6744f4b895b8056ba55dc46a2f5f427865
Hendi Santika <hendisantika@gmail.com>
2018-10-05 04:59:39 +0700

    #6 - Add User Repository

---
+++ src/main/java/com/hendisantika/springbootjwtauthentication/repository/UserRepository.java
@@ -0,0 +1,24 @@
+package com.hendisantika.springbootjwtauthentication.repository;
+
+import com.hendisantika.springbootjwtauthentication.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-jwt-authentication
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 05/10/18
+ * Time: 04.59
+ * To change this template use File | Settings | File Templates.
+ */
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+
+    Boolean existsByUsername(String username);
+
+    Boolean existsByEmail(String email);
+}
