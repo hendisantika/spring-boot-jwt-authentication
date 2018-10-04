2595eb7d21adfebea798ae4796920300247f8ea3
Hendi Santika <hendisantika@gmail.com>
2018-10-05 04:53:33 +0700

    #1 - first commit

---
+++ src/main/java/com/hendisantika/springbootjwtauthentication/SpringBootJwtAuthenticationApplication.java
@@ -0,0 +1,12 @@
+package com.hendisantika.springbootjwtauthentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJwtAuthenticationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootJwtAuthenticationApplication.class, args);
+    }
+}
