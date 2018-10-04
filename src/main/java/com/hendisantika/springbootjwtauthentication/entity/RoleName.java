98707ed60ef3900781b337b501ebc25f20e95eb9
Hendi Santika <hendisantika@gmail.com>
2018-10-05 04:57:07 +0700

    #3 - Add Role Name Enum

---
+++ src/main/java/com/hendisantika/springbootjwtauthentication/entity/RoleName.java
@@ -0,0 +1,17 @@
+package com.hendisantika.springbootjwtauthentication.entity;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-jwt-authentication
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 05/10/18
+ * Time: 04.56
+ * To change this template use File | Settings | File Templates.
+ */
+public enum RoleName {
+    ROLE_USER,
+    ROLE_PM,
+    ROLE_ADMIN
+}
